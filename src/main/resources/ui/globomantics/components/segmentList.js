a81b9357f2a97318e70da894a02403f7adb445ea
dkeysell <dkeysell@cisco.com>
2022-11-25 17:09:24 +0000

    Added form for entering coordinates

--- src/main/resources/ui/globomantics/components/segmentList.js
+++ src/main/resources/ui/globomantics/components/segmentList.js
@@ -3,11 +3,15 @@ import {useState, useEffect} from 'react';
 
 
 const SegmentList = ({segments, setSegments}) => {
-    const querySegments = () => {
-        
-        const swLat = 52.4;
-        const swLong = -0.7;
-        const size = 2;
+    const[coordinates, setCoordinates] = useState({swLat: "lat", swLong: "long", size: "size"});
+    const submit = (e) => {
+       e.preventDefault();
+        querySegments(coordinates)
+    };
+    const querySegments = (coordinates) => {
+        const swLat = coordinates.swLat;
+        const swLong = coordinates.swLong;
+        const size = coordinates.size;
         
         fetch("http://localhost:8080/api/v1/grid/discover?swLat="+swLat+"&swLong="+swLong+"&size="+size)
             .then(response => response.json()).then((data) => { setSegments(data);
@@ -17,12 +21,34 @@ const SegmentList = ({segments, setSegments}) => {
     
     return (
         <>
-        
+            <form onSubmit={submit}>
+                <input type="text" value={coordinates.swLat}
+                       onChange={(e)=> setCoordinates({...coordinates, swLat: e.target.value})}/>
+                <input type="text" value={coordinates.swLong}
+                       onChange={(e)=> setCoordinates({...coordinates, swLong: e.target.value})}/>
+                <input type="text" value={coordinates.size}
+                       onChange={(e)=> setCoordinates({...coordinates, size: e.target.value})}/>
+                <button className="btn btn-primary">Query segments</button>
+            </form>
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>Name</th>
                         <th>QoM Watts</th>
+                        <th>KoM Watts</th>
+                        <th>Distance</th>
+                        <th>Av Grade</th>
+                        <th>Max Grade</th>
+                        <th>Elevation Low</th>
+                        <th>Elevation High</th>
+                        <th>QoM</th>
+                        <th>KoM</th>
+                        <th>Athlete Count</th>
+                        <th>Effort Count</th>
+                        <th>Climb Cat</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Country</th>
                     </tr>
                 </thead>
                 
@@ -31,10 +57,8 @@ const SegmentList = ({segments, setSegments}) => {
                 </tbody>
 
             </table>
-
-            <button className="btn btn-primary" onClick={querySegments}>
-                Query segments
-            </button>
+            
+            
             
         </>
     );
--- src/main/resources/ui/globomantics/components/segmentRow.js
+++ src/main/resources/ui/globomantics/components/segmentRow.js
@@ -8,6 +8,20 @@ const SegmentRow = ({segment}) => {
         <tr onClick={() => navigate(navValues.segment, segment)}>
             <td>{segment.name}</td>
             <td>{segment.qomWatts}</td>
+	<td>{segment.komWatts}</td>
+	<td>{segment.distance}</td>
+	<td>{segment.average_grade}</td>
+	<td>{segment.maximum_grade}</td>
+	<td>{segment.elevation_low}</td>
+	<td>{segment.elevation_high}</td>
+	<td>{segment.qom}</td>
+	<td>{segment.kom}</td>
+	<td>{segment.athlete_count}</td>
+	<td>{segment.effort_count}</td>
+	<td>{segment.climb_category}</td>
+	<td>{segment.city}</td>
+	<td>{segment.state}</td>
+	<td>{segment.country}</td>
         </tr>
     );
 }
